d6aa99d9034ce4d94dd6aa20f28af271cbb2a33b
AkilaImashi <rgakilaimashigmail.com>
2026-07-21 14:12:13 +0530

    Initial springboot setup with Aiven database connection

---
+++ src/main/java/com/jobportal/job_portal/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.jobportal.job_portal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Job Portal backend is working!";
+    }
+}
