a881df818b01936d9806660a76f1a170960d7b1c
AkilaImashi <rgakilaimashigmail.com>
2026-07-23 16:26:35 +0530

    Add student profile endpoints

--- src/main/java/com/jobportal/job_portal/config/SecurityConfig.java
+++ src/main/java/com/jobportal/job_portal/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/test", "/api/auth/**", "/error").permitAll()
+                        .requestMatchers("/api/test", "/api/auth/**","/api/students/**", "/error").permitAll()
                         .anyRequest().authenticated())
                 .build();
     }
